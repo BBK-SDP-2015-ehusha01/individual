b3448c6d2b4c55000e97f798bce52306a3ee67c0
Elmario <elmario@wizardsworkshop.co.uk>
2015-01-25 14:41:13 +0000

    Added Mul instruction

--- src/sml/Translator.java
+++ src/sml/Translator.java
@@ -92,6 +92,11 @@ public class Translator {
 			r = scanInt();
 			s1 = scanInt();
 			return new LinInstruction(label, r, s1);
+		case "mul":
+			r = scanInt();
+			s1 = scanInt();
+			s2 = scanInt();
+			return new MulInstruction(label, r, s1, s2);
 		}
 
 		// You will have to write code here for the other instructions.
